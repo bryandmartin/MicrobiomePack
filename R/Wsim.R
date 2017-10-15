ad5142cfa4a7a896a3c0c0b20b80cd2c4a104af7
martinbryan <bryandmartin1@gmail.com>
2017-10-14 17:54:38 -0700

    Sigma to sigma

--- R/Wsim.R
+++ R/Wsim.R
@@ -22,14 +22,14 @@ Wsim <- function(out, W, X = NULL, niter = 1000) {
     if (is.vector(mu)) {
         for (i in 1:niter) {
             # set up Y take mu, sigma, simulate N Y_i's
-            Y.m <- mvrnorm(n = N, mu = mu, Sigma = out$sigma)
+            Y.m <- rmvn(n = N, mu = mu, sigma = out$sigma)
             W.m[, , i] <- YtoW(Y = Y.m, M = M, base = base)
         }
     }
     if (is.matrix(mu)) {
         for (i in 1:niter) {
             # apply out as vector stores as columns, transpose
-            Y.m <- t(apply(mu, 1, function(x) mvrnorm(n = 1, mu = x, Sigma = out$sigma)))
+            Y.m <- t(apply(mu, 1, function(x) rmvn(n = 1, mu = x, sigma = out$sigma)))
             W.m[, , i] <- YtoW(Y = Y.m, M = M, base = base)
         }
     }
