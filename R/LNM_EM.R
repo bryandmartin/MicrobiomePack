5efcb4ae400be22f89f54d9ffd4446d4bb54c6f2
martinbryan <bryandmartin1@gmail.com>
2018-01-15 11:17:31 -0800

    Add intercept checker

--- R/LNM_EM.R
+++ R/LNM_EM.R
@@ -23,6 +23,11 @@ LNM.EM <- function(W, X, base, EMiter = 10, EMburn = 5, MCiter = 1000, MCburn = 
     ### ROWS COLUMNS N is samples, Q is OTUs
     N <- nrow(W)
     Q <- ncol(W)
+    # Check for intercept, scaling means shouldn't need
+    checkInt <- function(x) all(x == 1)
+    IntInd <- apply(X, 2, checkInt)
+    X <- X[,-IndInd]
+      
     X <- scale(X)
     # purturbed Y (N x Q-1) - function in getData.R
     Y.p <- logratios(W, base = base, p = p)
